03a3f56fcfa6f811ea72bf44659bf4c137fa5a7c
CruuzAzul <alves.mckl@gmail.com>
2024-04-23 11:26:34 +0200

    chore : init poule #1

---
+++ config/city.template.ts
@@ -0,0 +1,39 @@
+import { TemplateInformations } from './templates';
+
+export const CityTemplate: TemplateInformations = {
+    eventId: 1,
+    eventName: 'Code In The Dark #2',
+    referenceImage: '/templates/city/page.jpg',
+    instructions: `
+--- The rules ---
+
+1) No previews - of either results or assets!
+2) Stay in this editor at all times
+3) No measurement tools
+4) Stop coding when the time's up
+5) After the round is over, press "Finish"
+
+Good luck and most important of all : have fun ! 🥳
+
+--- Assets ---
+
+⚠️ Remember to resize images if necessary 
+⚠️ Beware of file extensions !
+⚠️ All images have straight borders, no radius !
+
+img-bellecour.jpeg (w 412 x h 597)
+
+--- Font ---
+
+'Helvetica' is loaded
+
+--- Colors ---
+
+grey : #333333
+blue : #D2EAF4
+orange : #EFC48F
+red : #EECFCD
+`,
+    showPreview: true,
+    demoMode: true,
+};
