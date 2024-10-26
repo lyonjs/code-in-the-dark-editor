f7de909d9c4a26e6440012575506b82c02302bd2
Nirina Rabeson <hello@nirinarabeson.fr>
2024-10-26 10:29:25 +0200

    fix: adds image optimization from appwrite bucket

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+        pathname: '/v1/storage/buckets/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
