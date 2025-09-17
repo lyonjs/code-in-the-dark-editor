125da88554d4d94ebc956179a9caa55dc18c2459
malves.externe <malves.externe@bedrockstreaming.com>
2025-09-17 13:55:25 +0200

    feat: add form password and keep training templates

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Page() {
   const [selectedTemplate, setSelectedTemplate] = useState<TemplateName>(
     TemplateName.CITD
   );
+  const [isTrainningSession, setIsTrainningSession] = useState<boolean>(false);
   const { entry, updateFullName, updateId, updateIsLoading, updateTemplate } =
     useEntryStore();
   const { register, handleSubmit, formState } = useForm({
@@ -23,7 +24,7 @@ export default function Page() {
   const onSubmit = async (data: { [x: string]: any }) => {
     updateIsLoading(true);
     updateFullName(data.fullName);
-    updateTemplate(data.templateName);
+    updateTemplate(isTrainningSession ? selectedTemplate : data.templateName );
 
     // TODO : Create user in DB
 
@@ -51,14 +52,49 @@ export default function Page() {
           className={formState.errors.fullName ? styles.isWizz : ''}
           required
         />
-        <h3>Please set session code</h3>
-        <input
-          type='password'
-          placeholder='Session Password'
-          {...register('templateName', { required: true, max: 80 })}
-          className={formState.errors.fullName ? styles.isWizz : ''}
-          required
-        />
+        <div className={styles.trainingToggle}>
+          <input
+            id='toggle-template-select'
+            type='checkbox'
+            checked={isTrainningSession}
+            onChange={(e) => setIsTrainningSession(e.target.checked)}
+          />
+          <label htmlFor='toggle-template-select'>Check for training session</label>
+        </div>
+
+        {isTrainningSession ? (
+          <>
+            <h3>Select a template</h3>
+            <select
+              value={selectedTemplate}
+              onChange={(e) => setSelectedTemplate(e.target.value as TemplateName)}
+            >
+              {Object.keys(templatesDictionary).map((name) => (
+                <option key={name} value={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            <Image
+              priority
+              src={templatesDictionary[selectedTemplate].referenceImage}
+              alt='Image template reference'
+              width={200}
+              height={200}
+            />
+          </>
+        ) : (
+          <>
+            <h3>Please set session code</h3>
+            <input
+              type='text'
+              placeholder='Session Password'
+              {...register('templateName', { required: !isTrainningSession, max: 80 })}
+              className={formState.errors.fullName ? styles.isWizz : ''}
+              required={!isTrainningSession}
+            />
+          </>
+        )}
         <input type='submit' className='button' />
       </form>
     </>
